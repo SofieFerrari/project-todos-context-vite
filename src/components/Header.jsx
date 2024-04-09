e03a0e0027492a674a993a6e60a5f788bf3ce139
SofieFerrari <sofiestrahl@gmail.com>
2024-04-09 21:55:09 +0200

    minor minor styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 export const Header = () => {
 	return (
 		<>
-			<h1>Ta med det här!</h1>
-			<h2>till och från förskolan</h2>
-			<h3>checklistan för trötta föräldrar</h3>
+			<h1>Kom ihåg att ta med till förskolan!</h1>
 		</>
 	)
 }
