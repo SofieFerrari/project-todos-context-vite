5aaa9f5d88e2f7ec35e6c9df938947bd284c6ec0
SofieFerrari <sofiestrahl@gmail.com>
2024-04-13 22:11:26 +0200

    created public file

--- src/contexts/CheckListContext.jsx
+++ src/contexts/CheckListContext.jsx
@@ -6,11 +6,8 @@ export const CheckListProvider = ({ children }) => {
   const [itemsData, setItemsData] = useState([])
 
   //chatgpts kod, vad ska jag ha som argument? är det text.value eller ngt sånt? ta bort someItem från array?
-  const addToList = () => {
-  setItemsData(currentItems => [...currentItems, someItem])
-
-  // const addToList = (someItem) => {
-  //   setBringItems(...bringItems, someItem)
+  const addToList = (newItem) => {
+  setItemsData(prevItems => [...prevItems, newItem])
 
   }
 
